91b57ba62a08f275cfd347cdb99a207f11816852
Louis Afful <louisafful1@gmail.com>
2025-06-25 20:32:55 +0000

    chore: add pharmacist to the role

--- backend/models/userModels.js
+++ backend/models/userModels.js
@@ -29,7 +29,7 @@ const userSchema = mongoose.Schema(
     role: {
       type: String,
       default: "staff",
-      enum: ["staff", "admin", "supervisor"],
+      enum: ["staff", "admin", "supervisor", "pharmacist"],
     },
     active: {
       type: Boolean,
